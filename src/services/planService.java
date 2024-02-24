4b1a737a588ee3f7113166ab5062ec3592524993
Mariem Bejaoui <bejaoui.mariem@esprit.tn>
2024-02-24 14:44:02 +0100

    test 1

--- src/services/planService.java
+++ src/services/planService.java
@@ -18,7 +18,7 @@ public class planService {
     Connection cnx;
     notification nf = new notification();
     //  private EntityManager emf;
-
+//////////////////::testttt
     public planService() {
         cnx = MyConnexion.getInstance().getCnx();
     }
